db49014954f90a032671dd6f43f2b97741c53e5c
Frank Pham <pham-frank@gmx.de>
2019-12-17 12:25:47 +0100

    uploadButton für Camera

--- SneakerApp/ViewController/Profil/CameraPreviewViewController.swift
+++ SneakerApp/ViewController/Profil/CameraPreviewViewController.swift
@@ -6,21 +6,44 @@
 //  Copyright © 2019 Dung. All rights reserved.
 //
 import UIKit
-class CameraPreviewViewController: UIViewController {
-    
+class CameraPreviewViewController: UIViewController,UIImagePickerControllerDelegate {
     var image: UIImage!
     @IBOutlet weak var photo: UIImageView!
     @IBAction func saveButton(_ sender: UIButton) {
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
         dismiss(animated: true, completion: nil)
     }
     @IBAction func cancelButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func uploadButton(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else{
+            let alert = UIAlertController(title: "Warnung", message: "Du hast keine Berechtigung auf die Gallery", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         photo.image = self.image
+        
+        /*
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        pickerController.allowsEditing = true
+        pickerController.mediaTypes = ["public.image"]
+        pickerController.sourceType = .camera */
         // Do any additional setup after loading the view.
     }
     
