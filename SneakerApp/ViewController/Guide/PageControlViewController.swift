19b2c665d109d3c1c225cfa93b2881290e622942
Dung <duuung1998@gmail.com>
2020-01-09 21:19:05 +0100

    add views

---
+++ SneakerApp/ViewController/Guide/PageControlViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PageControlViewController.swift
+//  SneakerApp
+//
+//  Created by Dung  on 09.01.20.
+//  Copyright © 2020 Dung. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class PageControlViewController : UIPageViewController{
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         
+    }
+    
+
+    
+}
